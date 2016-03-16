8e850dbe757baf086dffbc0d46553e0f07c51e6a
Allen17 <allenwong98@gmail.com>
2016-03-16 14:08:58 -0400

    init InternationalSpaceStation project

---
+++ InternationalSpaceStation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InternationalSpaceStation
+//
+//  Created by Allen Wong on 3/16/16.
+//  Copyright © 2016 Allen Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
